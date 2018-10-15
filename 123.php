57c3131ed46391dc9f855c5c96c68e0c86081af0
yanruichengboke1991 <2296285375@qq.com>
2018-10-15 11:43:24 +0800

    d 123.php

--- 123.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-    function couponCODE(){
-            $aif_array=array("A","B","C","D","E","F","G","H","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
-            return $aif_array[array_rand($aif_array)].rand(100,999).$aif_array[array_rand($aif_array)].rand(100,999).$aif_array[array_rand($aif_array)].rand(100,999);
-        }
-
-
-        echo couponCODE();
