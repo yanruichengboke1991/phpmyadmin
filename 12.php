553c1f8f291c53536e3d4be71f718762d081341c
yanruichengboke1991 <2296285375@qq.com>
2018-10-15 10:11:41 +0800

    this is my test profile

--- 12.php
+++ 12.php
@@ -4,7 +4,9 @@ header("content-type:text/html;charset=utf-8");
 
 $str = '{"pageFooterLinks":"e38080","textShadow":"none","pageFooter":"e38080","pageHeaderLinks":"#F7D8D8","pageHeader":"#F7D8D8","fontLinkcolor":"#FFFFFF","fontcolor":"#F7D8D8","bordercolor":"#9B0000","fontBottomBgurl":"","fontborderRight":"true","fontborderBottom":"true","fontborderLeft":"true","fontborderTop":"true","fontborderColor":"#9B0000","fontborder":"true","font2Bgurl":"null","fontBgurl":"","fontBgcolor":"#C70000","fontHeadBgurl":"","bgurl":"","bgcolor":"#A80000","type":"sys","id":"","name":"undefined"}';
 
+echo '输出增加几行内容';
 
+echo 111111;die();
 $b = mysql_real_escape_string($str);
 
 echo $b;
